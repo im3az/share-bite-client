ff27a20412d6e050bbfe5047a6b647fbd4327b37
Imtiaz <im3azchowdhury@gmail.com>
2023-11-09 01:04:28 +0600

    single food details page style edited

--- src/pages/availableSingleFoodDetail/SingleFoodDetails.jsx
+++ src/pages/availableSingleFoodDetail/SingleFoodDetails.jsx
@@ -48,81 +48,57 @@ const SingleFoodDetails = () => {
           alt={foodName}
           className="w-full max-h-[600px] object-cover"
         />
-        <h1 className="text-5xl font-bold my-4 text-[#F017B8]">{foodName}</h1>
+        <h1 className="text-3xl md:text-5xl font-bold my-4 text-[#F017B8]">{foodName}</h1>
         <p className="text-gray-700 text-xl">{additionalNotes}</p>
       </div>
 
       <div className="bg-white p-6 shadow-md mt-4 flex flex-col md:flex-row justify-evenly">
         <div className="flex flex-col w-full">
-          <div className="grid card bg-base-300 p-10 rounded-box ">
+          <div className="flex flex-col w-full lg:flex-row justify-evenly bg-base-300 p-10 rounded-box ">
             <div>
               <h2 className="text-3xl font-bold mb-2 text-[#327DA0]">
                 Food Details
               </h2>
 
               <div className="text-lg">
-                <h2 className="font-semibold my-1">Quantity: {foodQuantity}</h2>
-                <h2 className="font-semibold my-1">Pickup Location:{pickupLocation}</h2>
-                <h2 className="font-semibold my-1">Expiration Date/Time:{expiredDateTime}</h2>
+                <h2 className=" my-1">
+                  <strong>Quantity: </strong> {foodQuantity}
+                </h2>
+                <h2 className=" my-1">
+                  <strong>Pickup Location: </strong>
+                  {pickupLocation}
+                </h2>
+                <h2 className=" my-1">
+                  <strong>Expiration Date: </strong> {expiredDateTime}
+                </h2>
               </div>
             </div>
-          </div>
-          <div className="divider"></div>
-          <div className="grid card bg-base-300 p-10 rounded-box ">
+
+            <div className="divider lg:divider-horizontal"></div>
+
             <div>
               <h2 className="text-3xl font-bold mb-2 text-[#327DA0]">
                 Donator Information
               </h2>
-              <div className="flex items-center  font-semibold my-1">
+              <div className="flex items-center  my-1">
                 <img
                   src={donatorImage}
                   alt={donatorName}
                   className="w-16 h-16 object-cover rounded-full mr-2"
                 />
                 <div>
-                  <p>Name: {donatorName}</p>
-                  <p>Email: {donatorEmail}</p>
+                  <p>
+                    <strong>Name:</strong> {donatorName}
+                  </p>
+                  <p>
+                    <strong>Email:</strong> {donatorEmail}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
+          <button className="btn btn-secondary w-1/2 mx-auto my-6">Request</button>
         </div>
-
-        {/* <div>
-          <h2 className="text-2xl font-semibold mb-2">Food Details</h2>
-          <ul>
-            <li>
-              <strong>Quantity:</strong> {foodQuantity}
-            </li>
-            <li>
-              <strong>Pickup Location:</strong> {pickupLocation}
-            </li>
-            <li>
-              <strong>Expiration Date/Time:</strong> {expiredDateTime}
-            </li>
-          </ul>
-        </div> */}
-
-        {/* <div>
-          <h2 className="text-2xl font-semibold mt-4 mb-2">
-            Donator Information
-          </h2>
-          <div className="flex items-center">
-            <img
-              src={donatorImage}
-              alt={donatorName}
-              className="w-10 h-10 object-cover rounded-full mr-2"
-            />
-            <div>
-              <p>
-                <strong>Name:</strong> {donatorName}
-              </p>
-              <p>
-                <strong>Email:</strong> {donatorEmail}
-              </p>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
